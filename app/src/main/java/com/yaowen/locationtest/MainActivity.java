c536bdc06219dc983f354640db8ab312066d1a29
YaoWen <yaowen424@126.com>
2015-10-30 14:21:44 +0800

    完成了所有代码了

--- app/src/main/java/com/yaowen/locationtest/MainActivity.java
+++ app/src/main/java/com/yaowen/locationtest/MainActivity.java
@@ -1,13 +1,107 @@
 package com.yaowen.locationtest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView LBSTextView;
+    private LocationManager manager;
+    private String provider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LBSTextView = (TextView) findViewById(R.id.LBSTextView);
+        manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        // 获取所有可用的位置提供器
+        List<String> providerList = manager.getProviders(true);
+        if (providerList.contains(LocationManager.GPS_PROVIDER)) {
+            provider = LocationManager.GPS_PROVIDER;
+        } else if
+                (providerList.contains(LocationManager.NETWORK_PROVIDER)) {
+            provider = LocationManager.NETWORK_PROVIDER;
+        } else {
+            // 当没有可用的位置提供器时，弹出Toast提⽰用户
+            Toast.makeText(this, "No location provider to use",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    public void requestPermissions(@NonNull String[] permissions, int requestCode)
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for Activity#requestPermissions for more details.
+            return;
+        }
+        Location location = manager.getLastKnownLocation(provider);
+        if (location != null) {
+            // 显⽰当前设备的位置信息
+            showLocation(location);
+        }
+        manager.requestLocationUpdates(provider, 5000, 1, locationListener);
+    }
+
+    private int checkSelfPermission(String accessFineLocation) {
+        return 0;
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        if (manager != null) {
+            // 关闭程序时将监听器移除
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    public void requestPermissions(@NonNull String[] permissions, int requestCode)
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for Activity#requestPermissions for more details.
+                return;
+            }
+            manager.removeUpdates(locationListener);
+        }
+    }
+
+    LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle
+                extras) {
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+        }
+
+        @Override
+        public void onLocationChanged(Location location) {
+            // 更新当前设备的位置信息
+            showLocation(location);
+        }
+    };
+
+    private void showLocation(Location location) {
+        String currentPosition = "latitude is " + location.getLatitude()
+                + "\n"
+                + "longitude is " + location.getLongitude();
+        LBSTextView.setText(currentPosition);
     }
 }
